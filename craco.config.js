b82eadc0c609dbfa188a5572b15dc146f292e92a
Fendy <zf@fendy5.cn>
2022-01-22 15:08:28 +0800

    build: 配置开发环境

--- craco.config.js
+++ craco.config.js
@@ -1,6 +1,24 @@
 const CracoLessPlugin = require('craco-less')
 
 module.exports = {
+  devServer: {
+    proxy: {
+      '/image-api': {
+        target: 'https://image.fendy5.cn/api/v1/upload',
+        changeOrigin: true,
+        pathRewrite: {
+          '^/image-api': ''
+        }
+      },
+      '/dev-api': {
+        target: 'http://127.0.0.1:6355',
+        changeOrigin: true,
+        pathRewrite: {
+          '^/dev-api': ''
+        }
+      }
+    }
+  },
   plugins: [
     {
       plugin: CracoLessPlugin,
